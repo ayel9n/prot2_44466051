a3e3c649eefe8dca1d1e91ebdf43c5ccc550744d
ayel9n <baezayelen25@gmail.com>
2025-05-29 00:04:52 -0300

    Primer commit del nuevo proyecto

---
+++ head_view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi página PHP</title>
+</head>
+<body>
+    <!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+<!--  -->
+<!-- jQuery y Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+<!--  -->
+</body>
+</html>
